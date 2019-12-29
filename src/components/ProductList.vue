92e82446238de846c1875098111f612d190a1f5a
xenia <adri.espanol@gmail.com>
2019-12-29 15:37:55 +0300

    Init

---
+++ src/components/ProductList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>Product List</h1>
+        <img v-if="loading" src="../assets/25.gif" />
+        <ul v-else>
+            <li v-for="product in products" :key="product.id">
+                {{product.title}} - {{product.price | currency}} - {{product.inventory}}
+                <button @click="addProductToCart(product)"> Add to cart</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loading: false
+      }
+    },
+
+    methods: {
+      addProductToCart(product) {
+        this.$store.dispatch('addProductToCart', product)
+      }
+    },
+
+    computed: {
+       products() {
+         return this.$store.getters.availableProducts
+       }
+     },
+
+    created() {
+      this.loading = true;
+       this.$store.dispatch('fetchProducts').then(() => this.loading = false)
+      }
+    }
+</script>
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>shopping cart</h1>
+        <ul>
+            <li v-for="product in products" :key="product.id">
+                {{product.title}} -- {{product.price | currency}} -- {{product.quantity}}
+            </li>
+        </ul>
+        <p>Total: {{total | currency}}</p>
+        <button @click="$store.dispatch('checkout')">Checkout</button>
+        <p>{{$store.state.checkoutStatus}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+      computed: {
+        products() {
+          return this.$store.getters.cartProducts
+        },
+
+        total() {
+          return this.$store.getters.cartTotal
+        }
+      }
+    }
+</script>
